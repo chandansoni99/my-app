b6294e50e6bed970c9aadaaeecd909328c2b055b
chandansoni99 <chandansoni.99@gmail.com>
2019-01-19 14:23:23 +0530

    Changes in app.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React from here
+            Learn React
           </a>
         </header>
       </div>
